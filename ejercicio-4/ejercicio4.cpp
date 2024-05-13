b15a17092502b87029f4a9c53d40c117bb439928
joaquinbian <joacobian21@gmail.com>
2024-05-13 01:30:13 -0300

    ejercicio 4, primeros 2 puntos

---
+++ ejercicio-4/ejercicio4.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+Dada una lista de números compuesta por 10 grupos y cada grupo separado del siguiente por un cero, se pide determinar e informar:
+
+a) Para cada uno de los grupos el máximo de los números pares y el máximo de los números impares. Se informan 2 resultados por cada grupo.
+b) Para cada uno de los grupos el porcentaje de números negativos y números positivos. Se informan 2 resultados por cada grupo.
+c) Cuántos números positivos había en total entre los 10 grupos. Se informa 1 resultado al final.
+
+
+*/
+
+
+int main(){
+
+    int GROUPS = 10;
+
+    int num;
+
+    int maxPar, maxImpar, countPos, countNeg;
+
+    for(int i = 0; i < 10; i++){
+
+      cout << endl;
+      cout << "Ingresa un numero diferente de 0 para agregarlo al GRUPO " << i + 1 <<  endl;
+      cout << "Para cambiar de grupo, ingrese 0 " << endl;
+      
+      cin >> num;
+
+      while(num != 0){
+
+        //calculamos el punto A
+        if((num % 2) == 0){
+            if(maxPar == 0 || num > maxPar){
+                maxPar = num;
+            }
+        } else {
+            if(maxImpar == 0 || num > maxImpar){
+                maxImpar = num;
+            }
+        }
+
+        //calculamos el punto b
+        if(num > 0){
+            countPos++;
+        } else {
+            countNeg++;
+        }
+
+      
+        cin >> num;
+      }
+        
+        cout << endl;
+        cout << "**** GRUPO " << i + 1 << " ****" << endl;
+        cout << "Maximo par -> " << maxPar << endl;
+        cout << "Maximo impar -> " << maxImpar << endl;
+        
+        int totalNums = countPos + countNeg;
+
+        cout << "Porcentaje positivos -> " << (countPos * 100) / totalNums << "%" << endl; 
+        cout << "Porcentaje negativos -> " << (countNeg * 100) / totalNums << "%" << endl; 
+
+
+        maxPar = 0;
+        maxImpar = 0;
+        countNeg = 0;
+        countPos = 0;
+    }
+
+
+
+    return 0;
+}
